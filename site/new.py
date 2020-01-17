07960087c45a267eacefd394dee6c151aab82d0e
wKenpachw <55124035+wKenpachw@users.noreply.github.com>
2020-01-17 23:06:22 +0500

    doc

--- site/new.py
+++ site/new.py
@@ -7,9 +7,9 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    with open(os.path.dirname(__file__)+'/data.txt', 'r', encoding='utf-8') as f:
+    #with open(os.path.dirname(__file__)+'/data.txt', 'r', encoding='utf-8') as f:
         #dataframe = pd.read_csv(f, sep='\t', names=['name', 'version', 'size', 'date'], parse_dates=[3])
-        exls = pd.read_excel(os.path.dirname(__file__)+'/data.xlsx', sheet_name='Лист1', names=['name', 'KD', 'HP', 'speed'])
+    exls = pd.read_excel(os.path.dirname(__file__)+'/data.xlsx', sheet_name='Лист1', names=['name', 'KD', 'HP', 'speed'])
     return flask.render_template('index.html', data=exls.iterrows())
 
 
@@ -17,13 +17,17 @@ def index():
 def char(name=None):
     return flask.render_template('char.html')
 
+@app.route('/char/<char_name>')
+def char_name(char_name):
+    return flask.render_template('char.html', char_name = char_name)
+
 @app.route('/chars/<int:post_id>')
 def show_post(post_id):
     return 'Post %d' % post_id
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(debug = True)
+    app.run(debug = True, host='0.0.0.0')
 
 
 
